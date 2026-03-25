6e6edc04be021bfc9d291588c0d73e4f608f3932
helenacristofoli <helenacristofoli@gmail.com>
2026-03-25 03:28:00 -0400

    Update Controls.h

--- Controls.h
+++ Controls.h
@@ -48,14 +48,14 @@ extern volatile float    ctrl_smooth[CTRL_COUNT];
  *   Q6 → Pitch      (LFO target)
  *   Q7 → Cutoff     (LFO target)
  * ================================================================ */
-#define SR_LED_SINE       0x02   // Q1
-#define SR_LED_SQUARE     0x04   // Q2
-#define SR_LED_TRIANGLE   0x08   // Q3
-#define SR_LED_SAW        0x10   // Q4
-#define SR_LED_AMPLITUDE  0x20   // Q5
-#define SR_LED_PITCH      0x40   // Q6
-#define SR_LED_CUTOFF     0x80   // Q7
-#define SR_LED_RESONANCE  0x01   // Q0
+#define SR_LED_SINE       0x02   // QB - pin 1
+#define SR_LED_SQUARE     0x01   // QA - pin 15
+#define SR_LED_TRIANGLE   0x08   // QD - pin 3
+#define SR_LED_SAW        0x04   // QC - pin 2
+#define SR_LED_AMPLITUDE  0x10   // QE - pin 4
+#define SR_LED_PITCH      0x20   // QF - pin 5
+#define SR_LED_CUTOFF     0x40   // QG - pin 6
+#define SR_LED_RESONANCE  0x80   // QH - pin 7
 
 /* ================================================================
  * BOTONES — Flags de evento
